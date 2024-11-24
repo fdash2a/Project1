595dd11de43ed132a1b77ec36e686745ffbe61d1
fdash2a <fdash2a@gmail.com>
2024-11-24 21:33:30 +0300

    Update get_random_anime.py

--- anime_adviser/get_random_anime.py
+++ anime_adviser/get_random_anime.py
@@ -1,10 +1,31 @@
-def _get_random_anime(genre, anime_type):
+def get_random_anime():
     global used_titles
-    if genre in anime_dict and anime_type in anime_dict[genre]:
-        anime_list = anime_dict[genre][anime_type]
-        available_anime = [anime for anime in anime_list if anime["title"] not in used_titles]
-        if available_anime:
-            selected_anime = random.choice(available_anime)
-            used_titles.add(selected_anime["title"])
-            return selected_anime
-    return
+    while True:
+        print("\nChoose what you want to experience:")
+        genre_choice = choose_item(anime_dict.keys(), "genre")
+        if genre_choice is None:
+            return
+        selected_genre = list(anime_dict.keys())[genre_choice]
+        print("\nDo you want to watch a film or a series?")
+        type_choice = choose_item(anime_dict[selected_genre].keys(), "type")
+        if type_choice is None:
+            return
+        selected_type = list(anime_dict[selected_genre].keys())[type_choice]
+
+        random_anime = None
+        if selected_genre in anime_dict and selected_type in anime_dict[selected_genre]:
+            anime_list = anime_dict[selected_genre][selected_type]
+            available_anime = [anime for anime in anime_list if anime["title"] not in used_titles]
+            if available_anime:
+                selected_anime = random.choice(available_anime)
+                used_titles.add(selected_anime["title"])
+                random_anime = selected_anime
+        if random_anime is not None:
+            print(
+                f"\nHere you go: {random_anime['title']}\n" + "\n".join(anime_info(random_anime)))
+        else:
+            print("You've already seen all the animes from this list.")
+
+        another = input("\nDo you want something else? (yes/no): ").strip().lower()
+        if another != 'yes':
+            return
